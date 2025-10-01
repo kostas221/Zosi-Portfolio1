f5d486f6fc77853e3c74a0f397d78f4f597e2aa7
kostas221 <109093715+kostas221@users.noreply.github.com>
2025-10-01 13:43:10 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  base:"/Zosi-Portfolio2",
+  base:"/Zosi-Portfolio1",
 });
